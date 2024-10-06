1daf2846c5f7f551714e3c913438080e6569d424
pi55man <cuinnv@gmail.com>
2024-10-06 15:24:33 +0530

    updated yet again (removed color column)

--- lua/pissman/set.lua
+++ lua/pissman/set.lua
@@ -3,7 +3,6 @@ vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
@@ -14,7 +13,7 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 
 vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = ""
 
 vim.g.mapleader = " "
 
