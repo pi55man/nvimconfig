91efda13dada6b886daead9f8c0c20d29e59909c
pi55man <cuinnv@gmail.com>
2024-11-04 06:51:47 +0530

    automatically closing brackets

--- lua/pissman/remap.lua
+++ lua/pissman/remap.lua
@@ -9,3 +9,10 @@ vim.keymap.set("v","K",":m '<-2<CR>gv=gv")
 
 vim.keymap.set("x","<leader>p","\"_dP")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Automatically close brackets
+vim.api.nvim_set_keymap('i', '(', '()<>Left>', { noremap = true })
+vim.api.nvim_set_keymap('i', '{', '{}<Left>', { noremap = true })
+vim.api.nvim_set_keymap('i', '[', '[]<Left>', { noremap = true })
+vim.api.nvim_set_keymap('i', "'", "''<Left>", { noremap = true })
+vim.api.nvim_set_keymap('i', '"', '""<Left>', { noremap = true })
