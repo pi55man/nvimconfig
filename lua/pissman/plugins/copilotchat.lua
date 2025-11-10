9a6884213f279972d31afbffbdc60a0d8a7e62d0
pi55man <cuinnv@gmail.com>
2025-11-10 14:46:24 +0000

    copilot extension

---
+++ lua/pissman/plugins/copilotchat.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      auto_insert_mode = true;
+
+    },
+  },
+}
